0049c4344444312634647c092f508e23891bfa3d
Patrick Eriksson <paleite@users.noreply.github.com>
2020-05-18 23:15:41 +0200

    Formatting

--- install.sh
+++ install.sh
@@ -21,14 +21,12 @@ dir_is_empty() {
   [[ $NUM_FILES -eq 0 ]]
 }
 
-if ! dir_is_empty
-then
+if ! dir_is_empty; then
   read -p "$(tput setaf 3)warning$(tput sgr0) This installer will create global git hook files in $(tput setaf 3)$(pwd)$(tput sgr0). Would you like to proceed? (y/$(tput bold)N$(tput sgr0)) " -n 1 -r
   echo ""
 
   # User didn't explicitly approve
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
   fi
 fi
@@ -37,7 +35,8 @@ trap 'rm -f $TEMP_PATH' EXIT
 
 readonly GIT_HOOKS=(applypatch-msg commit-msg post-applypatch post-checkout post-commit post-merge post-receive post-rewrite post-update pre-applypatch pre-auto-gc pre-commit pre-push pre-rebase pre-receive prepare-commit-msg push-to-checkout update)
 readonly TEMP_PATH=$(mktemp)
-readonly TEMPLATE=$(cat <<'END_OF_TEMPLATE'
+readonly TEMPLATE=$(
+  cat <<'END_OF_TEMPLATE'
 #!/usr/bin/env bash
 
 ########################################################
@@ -56,14 +55,12 @@ readonly GLOBAL_HOOK="${GLOBAL_DIR}/$HOOK.global.sh"
 readonly LOCAL_DIR="$(pwd)"
 readonly LOCAL_HOOK="${LOCAL_DIR}/${GIT_DIR}hooks/$HOOK"
 
-if [[ -s "$GLOBAL_HOOK" ]]
-then
+if [[ -s "$GLOBAL_HOOK" ]]; then
   echo "🌍  $HOOK hook"
   "$GLOBAL_HOOK" "$@"
 fi
 
-if [[ -s "$LOCAL_HOOK" ]]
-then
+if [[ -s "$LOCAL_HOOK" ]]; then
   echo "🏠  $HOOK hook"
   "$LOCAL_HOOK" "$@"
 fi
@@ -73,9 +70,8 @@ END_OF_TEMPLATE
 echo "📄  Copying template..."
 # Use an intermediate file and copy it, rather than overwriting the destination,
 # so as not to clobber pre-existing hooks.
-echo "${TEMPLATE}" > "${TEMP_PATH}"
-for GIT_HOOK in "${GIT_HOOKS[@]}";
-do
+echo "${TEMPLATE}" >"${TEMP_PATH}"
+for GIT_HOOK in "${GIT_HOOKS[@]}"; do
   cp "${TEMP_PATH}" "${GIT_HOOK}"
   touch "${GIT_HOOK}".global.sh
   chmod +x "${GIT_HOOK}" "${GIT_HOOK}".global.sh
